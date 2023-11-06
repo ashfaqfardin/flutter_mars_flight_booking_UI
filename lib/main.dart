9a7532459b633fdeae0a1c419290732ef4fc5f4d
ashfaqfardin <imashfaqfardin@gmail.com>
2023-11-06 19:11:04 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mars Ticket',
+      theme: ThemeData(scaffoldBackgroundColor: Color.fromRGBO(31, 31, 31, 1.0)),
+      home: Scaffold(),
+    );
+  }
+}
